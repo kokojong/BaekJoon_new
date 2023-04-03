8ba1326109bd3add58427aba4d8ebe40f58f4eab
kokojong <duwhd0349@kookmin.ac.kr>
2023-04-03 17:16:17 +0900

    20115 에너지 드링크

---
+++ BackJoon/20115_에너지 드링크.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  BackJoon
+//
+//  Created by kokojong on 2023/04/03.
+//
+
+//
+import Foundation
+// 20115 에너지 드링크
+// 5
+// 3 2 10 9 6
+
+/*
+ let n = Int(readLine()!)!
+ let input = readLine()!
+ 
+ var result: Double = 0.0
+ var arr: [Double] = input.components(separatedBy: " ").map { Double($0)! }.sorted()
+ //print(arr)
+ 
+ result = arr.last!
+ for i in 0..<n-1 {
+ result = result + arr[i] / 2
+ }
+ print(result)
+ */
---
+++ BackJoon/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  BackJoon
+//
+//  Created by kokojong on 2023/04/03.
+//
+
+import Foundation

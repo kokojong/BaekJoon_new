3b292ae7cf17b97ab78b9a28c1bd3b9ecdac5dee
kokojong <duwhd0349@kookmin.ac.kr>
2023-08-15 15:42:25 +0900

    2630 색종이 만들기

---
+++ WithPython/2630_색종이 만들기.py
@@ -0,0 +1,48 @@
+# 백준 2630 색종이 만들기 실2
+
+N = int(input())
+
+board = []
+
+for _ in range(N):
+    row = list(map(int, input().split()))
+    board.append(row)
+
+white = 0  # 0
+blue = 0  # 1
+
+# idea: N을 반씩 나눠가면서 성공한건 -1로 바꾸자
+
+
+def divide(row, column, n):
+    global white
+    global blue
+    now = board[row][column]  # 무슨색인지
+    isSame = True
+    for r in range(row, row + n):
+        for c in range(column, column + n):
+            if board[r][c] != now or board[r][c] == -1:
+                isSame = False
+                break
+
+    if isSame:
+        if now == 1:
+            blue += 1
+        elif now == 0:
+            white += 1
+
+        for r in range(row, row + n):
+            for c in range(column, column + n):
+                board[r][c] = -1
+    else:
+        k = n//2
+        divide(row, column, k)
+        divide(row + k, column, k)
+        divide(row, column + k, k)
+        divide(row + k, column + k, k)
+
+
+divide(0, 0, N)
+
+print(white)
+print(blue)
